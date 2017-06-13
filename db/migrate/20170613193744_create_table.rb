70c8ea2dc8bee024b2e0df978e4083498f26abb1
Lia Forgione <liaforg@gmail.com>
2017-06-13 15:39:55 -0400

    adding quant to parts and party invite

---
+++ db/migrate/20170613193744_create_table.rb
@@ -0,0 +1,15 @@
+class CreateTable < ActiveRecord::Migration[5.0]
+  def change
+    create_table :tables do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :diet
+      t.integer :salary
+      t.integer :kids
+      t.string :vulnerabilites
+      t.string :illnesses
+      t.string :medication
+      t.string :vote
+    end
+  end
+end
