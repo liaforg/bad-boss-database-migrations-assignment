6ed076b4e7ec18da2c7f24c990505776d0e89bae
Lia Forgione <liaforg@gmail.com>
2017-06-13 16:03:53 -0400

    change quantity from integer to decimal

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170613195511) do
+ActiveRecord::Schema.define(version: 20170613200020) do
 
   create_table "locations", force: :cascade do |t|
     t.string "country"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20170613195511) do
     t.string  "name"
     t.string  "manufacture"
     t.decimal "cost"
-    t.integer "quantity"
+    t.decimal "quantity"
   end
 
   create_table "widgets", force: :cascade do |t|
